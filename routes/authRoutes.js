259c90f15998c3435a8f4b9d3563a240c335a808
Awdhoot Lele <awadhoot.lele@synerzip.com>
2018-09-03 19:14:55 +0530

    Completed login and logout callback flows

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,26 +1,32 @@
 const passport = require('passport');
 
+module.exports = app => {
+  // invoke 'google' strategy when route is hit
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
 
-module.exports = (app) => {
-    // invoke 'google' strategy when route is hit
-    app.get(
-        '/auth/google',
-        passport.authenticate('google', {
-            scope: ['profile', 'email']
-        })
-    );
+  // when we get 'code' from URL, we authenticate it with passport.authenticate
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      // redirect to api which can be only called after successful authentication/login
+      res.redirect('/surveys');
+    }
+  );
 
+  app.get('/api/logout', (req, res) => {
+    req.logout(); // injected by passport -> kills the cookie associated with this request
+    // redirect to root (landing) page
+    res.redirect('/');
+  });
 
-    // when we get 'code' from URL, we authenticate it with passport.authenticate
-    app.get('/auth/google/callback', passport.authenticate('google'));
-
-    app.get('/api/logout', (req, res) => {
-        req.logout(); // injected by passport -> kills the cookie associated with this request
-        res.send(req.user);
-    });
-
-    app.get('/api/current_user', (req, res) => {
-        // passport attached the user property to req object (If we are using passport based cookie management)
-        res.send(req.user);
-    });
-}
+  app.get('/api/current_user', (req, res) => {
+    // passport attached the user property to req object (If we are using passport based cookie management)
+    res.send(req.user);
+  });
+};
