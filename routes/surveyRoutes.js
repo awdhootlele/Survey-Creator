a8510ce210dcd01f25b5a2ea595ac37e7737da97
Awdhoot Lele <awadhoot.lele@synerzip.com>
2018-09-07 10:59:22 +0530

    Updated Email template and added initial level parsing of sendGrid webhook data

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,9 @@
 // all survey routes are defined here
+
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
+
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const mongoose = require('mongoose');
@@ -6,7 +11,7 @@ const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys'); // instead of requiring the model directly (As it is already included inside index.js, we refer that model here)
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:feedback', (req, res) => {
     res.send(`<h3 style="text-align: center">Thanks for the response!!!</h3>`);
   });
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
@@ -35,5 +40,23 @@ module.exports = app => {
       // 422 -> unprocessable entity
       res.status(422).send(err);
     }
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    // preprocessing on webhook data ->
+    //  remove data which does not meet 1. url path of /api/surveys/ 2. has duplicate surveyID and email at same time
+    const events = _.map(req.body, event => {
+      const { email, url } = event;
+      const match = new Path('/api/surveys/:surveyId/:choice').test(
+        new URL(url).pathame
+      ); // returns an object with {surveyId, choice} if match found else returns null
+      // dont destructure from match oj, coz it could be null
+      if (match) {
+        return { email, surveyId: match.surveyId, choice: match.choice };
+      }
+      // else returns undefined automatically
+    });
+    const compactedEvents = _.compact(events); // removes all undefined events
+    const uniqueEvents = _.uniqBy(compactedEvents, 'email', 'surveyId'); // removes duplicate data, lookup by email and surveyId props at same time
   });
 };
