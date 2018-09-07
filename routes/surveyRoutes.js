6ec7a2872db8e0aabbf89eb2eb32eedc53482647
Awdhoot Lele <awadhoot.lele@synerzip.com>
2018-09-07 12:05:11 +0530

    Completed user feedback recording part - mongoose update survey query

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,7 +1,7 @@
 // all survey routes are defined here
 
 const _ = require('lodash');
-const Path = require('path-parser');
+const Path = require('path-parser').default;
 const { URL } = require('url');
 
 const requireLogin = require('../middlewares/requireLogin');
@@ -11,7 +11,7 @@ const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys'); // instead of requiring the model directly (As it is already included inside index.js, we refer that model here)
 module.exports = app => {
-  app.get('/api/surveys/:surveyId/:feedback', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send(`<h3 style="text-align: center">Thanks for the response!!!</h3>`);
   });
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
@@ -45,21 +45,47 @@ module.exports = app => {
   app.post('/api/surveys/webhooks', (req, res) => {
     // preprocessing on webhook data ->
     //  remove data which does not meet 1. url path of /api/surveys/ 2. has duplicate surveyID and email at same time
-    const events = _.chain(req.body)
-      .map(event => {
-        const { email, url } = event;
-        const match = new Path('/api/surveys/:surveyId/:choice').test(
-          new URL(url).pathame
-        ); // returns an object with {surveyId, choice} if match found else returns null
-        // dont destructure from match oj, coz it could be null
-        if (match) {
-          return { email, surveyId: match.surveyId, choice: match.choice };
-        }
-        // else returns undefined automatically
-      })
-      .compact() // removes all undefined events
-      .uniqBy('email', 'surveyId') // removes duplicate data, lookup by email and surveyId props at same time
-      .value();
+    // TODO - bug -> If user responds yes and then no immediately, database stores last choice which is 'no', but it should be 'yes' coz it was first one
+    try {
+      _.chain(req.body)
+        .map(event => {
+          const { email, url } = event;
+
+          const match = new Path('/api/surveys/:surveyId/:choice').test(
+            new URL(url).pathname
+          ); // returns an object with {surveyId, choice} if match found else returns null
+          // dont destructure from match oj, coz it could be null
+
+          if (match) {
+            return { email, surveyId: match.surveyId, choice: match.choice };
+          }
+          // else returns undefined automatically
+        })
+        .compact() // removes all undefined events
+        .uniqBy('email', 'surveyId') // removes duplicate data, lookup by email and surveyId props at same time
+        .each(event => {
+          // update Survey collection with the feedback
+          // we dont have to WAIT for the query to finish, hence no async await syntax here
+          const { surveyId, email, choice } = event;
+          // recipients.$.responded -> look into recipients array, take the exact index ($) of matched recipient-> update its 'responded' prop
+          Survey.updateOne(
+            {
+              _id: surveyId,
+              recipients: {
+                $elemMatch: { email: email, responded: false }
+              }
+            },
+            {
+              $inc: { [choice]: 1 },
+              $set: { 'recipients.$.responded': true },
+              lastResponded: new Date() // last responded to the current date
+            }
+          ).exec();
+        })
+        .value();
+    } catch (err) {
+      console.error(err);
+    }
     res.send({});
   });
 };
